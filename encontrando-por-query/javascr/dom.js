df65f9c4bf9cd1f4fec63723e9f05ec533feb098
Lindrielton silva <161902702+lindrielton@users.noreply.github.com>
2025-06-16 13:35:15 -0300

    queryselector

---
+++ encontrando-por-query/javascr/dom.js
@@ -0,0 +1,11 @@
+
+console.log(document.querySelector("#titulo"));
+console.log(document.querySelector(".elemento"));
+
+console.log(document.querySelector("footer h1"));
+
+console.log(document.querySelector("div p"));
+
+console.log(document.querySelector("div #titulo"));
+
+console.log(document.querySelector("ul .elemento"));
