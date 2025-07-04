da93432a709c86741fc0c6b7e350881032d6364b
Lindrielton silva <161902702+lindrielton@users.noreply.github.com>
2025-07-04 13:38:03 -0300

    eventos

---
+++ 19-primeiroEvento/javascr/dom.js
@@ -0,0 +1,14 @@
+
+
+let btn = document.querySelector("button");
+
+addEventListener("click",() => {
+    console.log("sejá bem vindo")
+});
+
+
+
+addEventListener("click", function(){
+     this.window.alert("sejá bem vindo")
+})
+
