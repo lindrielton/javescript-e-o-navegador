d11f6ba8ea6787ee2d4c53caed6add7bbbc4dd15
Lindrielton silva <161902702+lindrielton@users.noreply.github.com>
2025-07-07 06:34:01 -0300

    EVENTOS

--- 19-primeiroEvento/javascr/dom.js
+++ 19-primeiroEvento/javascr/dom.js
@@ -2,13 +2,13 @@
 
 let btn = document.querySelector("button");
 
-addEventListener("click",() => {
+btn.addEventListener("click",() => {
     console.log("sejá bem vindo")
 });
 
 
 
-addEventListener("click", function(){
+btn.addEventListener("click", function(){
      this.window.alert("sejá bem vindo")
 })
 
---
+++ 20-deletando-eventos/javascr/dom.js
@@ -0,0 +1,16 @@
+
+
+let btn = document.querySelector("#btn1");
+
+function msg(){
+    console.log('olá sejá bem vindo');
+}
+
+btn.addEventListener("click",msg);
+
+btn2.addEventListener("click", function(){
+    btn.removeEventListener("click", msg);
+})
+
+
+
